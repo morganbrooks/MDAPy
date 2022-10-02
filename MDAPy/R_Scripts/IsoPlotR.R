c0cd410522b7b5c3470660f30a74fb22bed3db51
Diego F. Coelho <diegofcoelho@gmail.com>
2022-10-02 18:22:01 -0300

    push fixes

--- MDAPy/R_Scripts/IsoPlotR.R
+++ MDAPy/R_Scripts/IsoPlotR.R
@@ -4,30 +4,39 @@ args = commandArgs(trailingOnly = TRUE)
 invisible({
   pkgs <- c("IsoplotR", "dplyr", "rjson", "openxlsx")
   installed <- (pkgs %in% rownames(installed.packages()))
-  if (!all(installed)) {install.packages(pkgs[!installed], quiet = TRUE, 
-                                         verbose = FALSE,
-                                         repos = "http://cran.us.r-project.org")}
+  if (!all(installed)) {
+    install.packages(pkgs[!installed],
+      quiet = TRUE,
+      verbose = FALSE,
+      repos = "http://cran.us.r-project.org"
+    )
+  }
   load_all_libraries <- lapply(pkgs, require, character.only = TRUE)
-  # note that the working directory is important here, otherwise you will need to
+  # note that the working directory is important here,
+  # otherwise you will need to
   # provide a full path to the folder containing the data.
-  file <- paste0(getwd(), "/", args[1]) 
+  file <- paste0(getwd(), "/", args[1])
   # we load the data 
   data <- read.xlsx(file, sheet = "Data")
   #
   samples <- rjson::fromJSON(args[2])
   #
-  peak_values <- sapply(samples, function(sample){
+  peak_values <- sapply(samples, function(sample) {
     png(filename = paste0("Saved_Files/MLA_Plots/plot_", sample, ".png"))
     mixtures <- data %>% filter(Sample_ID == sample) %>% select(-Sample_ID)
     # then we generate the plot
-    radialplot(mixtures, k = 'min', bg = 'cornflowerblue', transformation='log', sigdig=2,alpha=0.05)
+    radialplot(mixtures,
+      k = "min", bg = "cornflowerblue",
+      transformation = "log", sigdig = 2, alpha = 0.05
+    )
     # If you don't want the sample name written on the picture, delete the line
     # bellow
-    #title(main = sample, adj = 1, line = 2)
+    # title(main = sample, adj = 1, line = 2)
+    # mtext(sample, 2, adj = 5, las = 1, padj = -17)
     # save on disk
     invisible(dev.off())
     # and calculate the parameter, returning it to python
-    peakfit(mixtures, k = 'min', sigdig = 2,alpha=0.05)$peaks[1]
+    peakfit(mixtures, k = "min", sigdig = 2, alpha = 0.05)$peaks[1]
 })
     })
 #
--- R_Scripts/IsoPlotR.R
+++ R_Scripts/IsoPlotR.R
@@ -5,9 +5,13 @@ invisible({
     suppressPackageStartupMessages({
   pkgs <- c("IsoplotR", "dplyr", "rjson", "openxlsx", "readxl")
   installed <- (pkgs %in% rownames(installed.packages()))
-  if (!all(installed)) {install.packages(pkgs[!installed], quiet = TRUE, 
-                                         verbose = FALSE,
-                                         repos = "http://cran.us.r-project.org")}
+  if (!all(installed)) {
+    install.packages(pkgs[!installed],
+      quiet = TRUE,
+      verbose = FALSE,
+      repos = "http://cran.us.r-project.org"
+    )
+  }
   load_all_libraries <- lapply(pkgs, require, character.only = TRUE)
         })
   # note that the working directory is important here, otherwise you will need to
@@ -22,16 +26,69 @@ invisible({
   peak_values <- sapply(samples, function(sample) {
     mixtures <- data %>% filter(Sample_ID == sample) %>% select(-Sample_ID)
     # then we generate the plot
-    png(filename = paste0(getwd(), "/assets/plots/IsoplotR/plot_", sample, ".png"))
-    radialplot(as.data.frame(mixtures), k = 'min', bg = 'cornflowerblue', transformation='log', sigdig=2,alpha=0.05)
+    #########################################################################
+    png(filename = paste0(getwd(),
+     "/assets/plots/IsoplotR/plot_", sample, ".png"))
+    radialplot(as.data.frame(mixtures),
+      k = "min", bg = "cornflowerblue", transformation = "log",
+      sigdig = 2, alpha = 0.05
+    )
+    title(main = sample, adj = 0, line = 2)
     invisible(dev.off())
-    svg(filename = paste0(getwd(), "/assets/plots/IsoplotR/plot_", sample, ".svg"))
-    radialplot(as.data.frame(mixtures), k = 'min', bg = 'cornflowerblue', transformation='log', sigdig=2,alpha=0.05)
+    #########################################################################
+    jpeg(filename = paste0(getwd(),
+     "/assets/plots/IsoplotR/plot_", sample, ".jpeg"))
+    radialplot(as.data.frame(mixtures),
+      k = "min", bg = "cornflowerblue", transformation = "log",
+      sigdig = 2, alpha = 0.05
+    )
+    title(main = sample, adj = 0, line = 2)
     invisible(dev.off())
-    #title(main = sample, adj = 1, line = 2)
+    #########################################################################
+    tiff(filename = paste0(getwd(),
+     "/assets/plots/IsoplotR/plot_", sample, ".tiff"))
+    radialplot(as.data.frame(mixtures),
+      k = "min", bg = "cornflowerblue", transformation = "log",
+      sigdig = 2, alpha = 0.05
+    )
+    title(main = sample, adj = 0, line = 2)
+    invisible(dev.off())
+    #########################################################################
+    postscript(paste0(getwd(),
+     "/assets/plots/IsoplotR/plot_", sample, ".eps"))
+    radialplot(as.data.frame(mixtures),
+      k = "min", bg = "cornflowerblue", transformation = "log",
+      sigdig = 2, alpha = 0.05
+    )
+    title(main = sample, adj = 0, line = 2)
+    invisible(dev.off())
+    #########################################################################
+    pdf(paste0(getwd(),
+     "/assets/plots/IsoplotR/plot_", sample, ".pdf"))
+    radialplot(as.data.frame(mixtures),
+      k = "min", bg = "cornflowerblue", transformation = "log",
+      sigdig = 2, alpha = 0.05
+    )
+    title(main = sample, adj = 0, line = 2)
+    invisible(dev.off())
+    #########################################################################
+    svg(filename = paste0(
+      getwd(),
+      "/assets/plots/IsoplotR/plot_", sample, ".svg"
+    ))
+    radialplot(as.data.frame(mixtures),
+      k = "min", bg = "cornflowerblue", transformation = "log",
+      sigdig = 2, alpha = 0.05
+    )
+    title(main = sample, adj = 0, line = 2)
+    invisible(dev.off())
+    ##########################################################################
     # save on disk
     # and calculate the parameter, returning it to python
-   peakfit(as.data.frame(mixtures), k = 'min', sigdig = 2, alpha = 0.05)$peaks
+   peakfit(as.data.frame(mixtures),
+     k = "min",
+     sigdig = 2, alpha = 0.05
+   )$peaks
     }, simplify = FALSE, USE.NAMES = TRUE)
   })
 #
